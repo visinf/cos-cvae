1b593611fe6ee21f837913f655f151e4f31a1728
s-mahajan <s-mahajan@visinf.tu-darmstadt.de>
2020-10-26 11:56:46 +0100

    Patched Spice score.

--- GoogleConceptualCaptioning/cococaption/pycocoevalcap/spice/spice.py
+++ GoogleConceptualCaptioning/cococaption/pycocoevalcap/spice/spice.py
@@ -26,11 +26,6 @@ class Spice:
     Main Class to compute the SPICE metric 
     """
     def __init__(self):
-        #cwd = os.path.dirname(os.path.abspath(__file__))
-        #cache_dir=os.path.join(cwd, CACHE_DIR, str(time.time()))
-        #self.cache_dir = cache_dir
-        #if not os.path.exists(cache_dir):
-        #  os.makedirs(cache_dir)
         pass
 
     def float_convert(self, obj):
@@ -61,64 +56,36 @@ class Spice:
               "refs" : ref
             })
 
-        #cwd = os.path.dirname(os.path.abspath(__file__))
-        #temp_dir=os.path.join(cwd, TEMP_DIR)
-        #if not os.path.exists(temp_dir):
-        #  os.makedirs(temp_dir)
-        with open('/BS/apr/work/nocaps/image-feature-extractors/test1.json','w') as in_file:    
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        temp_dir=os.path.join(cwd, TEMP_DIR)
+        if not os.path.exists(temp_dir):
+          os.makedirs(temp_dir)
+
+        in_file_path = os.path.join(temp_dir, 'test1.json')
+        with open(in_file_path,'w') as in_file:    
           json.dump(input_data,in_file)
-        #in_file = './test1.json'#tempfile.NamedTemporaryFile(delete=False, dir=temp_dir)
-        #in_file.write(json.dumps(input_data, indent=2).encode('utf-8'))
-        #in_file.close()
 
-        # Start job
-        #out_file = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir)
-        #out_file.close()
+        out_file_path = os.path.join(temp_dir, 'tmp_results1.json')
 
-       # out = subprocess.run(['java', '-classpath','./SPICE-1.0/','-jar','-Xmx8G',
-       #'./SPICE-1.0/spice-1.0.jar','./test1.json','-cache','./spice_cache','-out',
-       #'tmp_results1.json','-subset','-silent'], stdout=subprocess.PIPE, stderr=FNULL)#
-  
-
-        spice_cmd = ['java','-classpath','/BS/apr/work/nocaps/image-feature-extractors/SPICE-1.0/', '-jar', 
-          '-Xmx8G', '/BS/apr/work/nocaps/image-feature-extractors/SPICE-1.0/spice-1.0.jar', '/BS/apr/work/nocaps/image-feature-extractors/test1.json',
-          '-out','/BS/apr/work/nocaps/image-feature-extractors/tmp_results1.json',
+        spice_cmd = ['java','-classpath',cwd, '-jar', 
+          '-Xmx8G', os.path.join(cwd,'spice-1.0.jar'), in_file_path,
+          '-out',out_file_path,
           '-subset', '-silent'
           
-        ]#'-silent' '-cache', self.cache_dir, '-cache','/BS/apr/work/nocaps/image-feature-extractors/spice_cache',
+        ]
         out = subprocess.run(spice_cmd, stdout=subprocess.PIPE, stderr=FNULL)#
-        #    cwd=os.path.dirname(os.path.abspath(__file__)))
-        #out = subprocess.run(['java', '-classpath','./SPICE-1.0/','-jar','-Xmx8G','./SPICE-1.0/spice-1.0.jar','./test1.json','-cache','./spice_cache','-out','tmp_results1.json','-subset','-silent'], stdout=subprocess.PIPE, stderr=FNULL)#
-        #print(out)
+       
 
         # Read and process results
-        with open('/BS/apr/work/nocaps/image-feature-extractors/tmp_results1.json') as data_file:    
+        with open(out_file_path) as data_file:    
           results = json.load(data_file)
-        #os.remove(in_file.name)
-        #os.remove(out_file.name)
 
-        scores = []#[None for _ in range(len(results))]
+        scores = []
         for item in results:
           scores.append(item['scores']['All']['f'])
 
         scores = np.array(scores)
         average_score = np.mean(scores)
-
-        '''imgId_to_scores = {}
-        spice_scores = []
-        for item in results:
-          imgId_to_scores[item['image_id']] = item['scores']
-          spice_scores.append(self.float_convert(item['scores']['All']['f']))
-        average_score = np.mean(np.array(spice_scores))
-        scores = []
-        for image_id in imgIds:
-          # Convert none to NaN before saving scores over subcategories
-          score_set = {}
-          for category,score_tuple in imgId_to_scores[image_id].items():
-            score_set[category] = {k: self.float_convert(v) for k, v in score_tuple.items()}
-          scores.append(score_set)
-        print('average_score ',average_score)
-        print('scores ',scores)'''
         return average_score, scores
 
     def method(self):
@@ -126,6 +93,5 @@ class Spice:
 
     def __del__(self):
         pass
-        #shutil.rmtree(self.cache_dir)
 
 
